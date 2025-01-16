c6173d310166f0293d3b925e0622c18e02cd39cd
JohannUM <johann.lottermoser@outlook.de>
2025-01-16 15:23:28 +0100

    added stuff

--- FilterGenesNoise.R
+++ FilterGenesNoise.R
@@ -4,11 +4,15 @@ options(stringsAsFactors = FALSE)
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 if (!requireNamespace("here", quietly = TRUE)) install.packages()("here")
 if (!requireNamespace("biomaRt", quietly = TRUE)) BiocManager::install("biomaRt")
+if (!requireNamespace("org.Hs.eg.db", quietly = TRUE)) BiocManager::install("org.Hs.eg.db")
+if (!requireNamespace("TxDb.Hsapiens.UCSC.hg38.knownGene", quietly = TRUE)) BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
 
 # Load the necessary libraries ------------------------------------------------
 
 library(here)
 library(biomaRt)
+library(org.Hs.eg.db)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
 # Load the data ------------------------------------------------
 
@@ -31,7 +35,7 @@ if (!all(rownames(data_expression) == rownames(data_exon_lengths))) {
 
 # Calculate expression above noise ------------------------------------------------
 
-ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+ensembl <- useEnsembl("ensembl", mirror = "useast", dataset = "hsapiens_gene_ensembl")
 
 gene_info <- getBM(
     attributes = c("ensembl_gene_id", "chromosome_name"),
@@ -42,6 +46,26 @@ gene_info <- getBM(
 
 rownames(gene_info) <- gene_info$ensembl_gene_id
 
+# Get gene symbols and Entrez IDs
+# annotations <- AnnotationDbi::select(
+#     org.Hs.eg.db,
+#     keys = rownames(data_log),
+#     columns = c("SYMBOL", "ENTREZID"),
+#     keytype = "ENSEMBL"
+# )
+# annotations <- annotations[!duplicated(annotations$ENSEMBL), ]
+# # Add chromosome information from TxDb
+# txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+
+# # Map Ensembl IDs to chromosome information
+# chromosome_data <- genes(txdb, columns = c("gene_id", "tx_name", "tx_chrom"))
+# chromosome_mapping <- as.data.frame(chromosome_data)
+
+# # Merge annotations with chromosome data
+# final_annotations <- merge(annotations, chromosome_mapping, by.x = "ENTREZID", by.y = "gene_id", all.x = TRUE)
+
+# rownames(final_annotations) <- final_annotations$ENSEMBL
+
 cpm2fpkm <- function(x) {
     exon_lengths_kb <- data_exon_lengths[, 1] / 1E3
     .t <- 2^(x) / exon_lengths_kb
@@ -50,6 +74,7 @@ data_expression_fpkm <- cpm2fpkm(data_log)
 
 row_means <- rowMeans(data_expression_fpkm)
 
+# genes_on_Y <- rownames(final_annotations[final_annotations$tx_chrom == "chrY", ])
 genes_on_Y <- rownames(gene_info[gene_info$chromosome_name == "Y", ])
 
 data_expression_fpkm_Y <- data_expression_fpkm[rownames(data_expression_fpkm) %in% genes_on_Y, ]
@@ -63,6 +88,8 @@ expression_above_noise <- as.data.frame(t(apply(data_expression_fpkm, 1, functio
     above_noise <- t_test$p.value < 0.05
     c(p_value = t_test$p.value, above_noise = above_noise)
 })))
+
+test <- expression_above_noise[expression_above_noise$above_noise == TRUE, ]
 
 data_log_filtered <- data_log[expression_above_noise$above_noise == TRUE, ]
 
