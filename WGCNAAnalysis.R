ea47435d047738ffaa81eeb21e4cb935117986ef
JohannUM <johann.lottermoser@outlook.de>
2025-01-23 23:19:48 +0100

    Added more visualization options

--- WGCNAAnalysis.R
+++ WGCNAAnalysis.R
@@ -6,6 +6,7 @@ if (!requireNamespace("here", quietly = TRUE)) install.packages()("here")
 if (!requireNamespace("WGCNA", quietly = TRUE)) BiocManager::install("WGCNA")
 if (!requireNamespace("clusterProfiler", quietly = TRUE)) BiocManager::install("clusterProfiler")
 if (!requireNamespace("org.Hs.eg.db", quietly = TRUE)) BiocManager::install("org.Hs.eg.db")
+if (!requireNamespace("RCy3", quietly = TRUE)) BiocManager::install("RCy3")
 
 # Load the necessary libraries ------------------------------------------------
 
@@ -14,6 +15,7 @@ library(WGCNA)
 library(ggplot2)
 library(clusterProfiler)
 library(org.Hs.eg.db)
+library(RCy3)
 
 allowWGCNAThreads()
 
@@ -69,7 +71,7 @@ heatmap <- labeledHeatmap(
 # Calculate module membership and gene significance --------------------------------------------------
 
 diabetes <- as.data.frame(trait_data$diabetes)
-names(diabates) <- "diabetes"
+names(diabetes) <- "diabetes"
 mod_names <- substring(names(module_eigengenes), 3)
 gene_module_membership <- as.data.frame(cor(data_expression, module_eigengenes, use = "p"))
 MMP_value <- as.data.frame(corPvalueStudent(as.matrix(gene_module_membership), n_samples))
@@ -97,11 +99,14 @@ verboseScatterplot(abs(gene_module_membership[module_genes, column]), abs(gene_t
 )
 
 # get the gene significance and module membership for the module of interest
+# look at this to object to see all genes in the module and its significance and membership
 gene_significance_membership <- data.frame(
     GeneSignificance = abs(gene_trait_significance[module_genes, 1]),
     ModuleMembership = abs(gene_module_membership[module_genes, column])
 )
 rownames(gene_significance_membership) <- rownames(gene_module_membership[module_genes, ])
+
+# Perform GO enrichment analysis ------------------------------------------------
 
 genes_module <- rownames(gene_significance_membership)
 
@@ -118,3 +123,75 @@ goResults <- enrichGO(
 head(goResults)
 sizeGrWindow(7, 7)
 dotplot(goResults, showCategory = 10, title = "GO Enrichment Analysis")
+
+# Plot TOM heatmap ------------------------------------------------
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Cant recommend this because it takes a lot of time and need to run WGCNA.R locally to have the TOM object (it is in the gitignore and wont be pushed)
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!
+# TOM <- readRDS(paste(etiology, "/TOM_", etiology, ".RDS", sep = ""))
+
+# diss_TOM <- 1 - TOM
+# plot_TOM <- diss_TOM^7
+# diag(plotTOM) <- NA
+
+# sizeGrWindow(15, 15)
+# TOMplot(plot_TOM, gene_tree, module_colors, main = "Network heatmap plot, all genes")
+
+# Plot eigengene adjacency ------------------------------------------------
+
+module_eigengenes_trait <- orderMEs(cbind(new_module_eigengenes, diabetes))
+
+# do this in one or next one seperate
+sizeGrWindow(11, 11)
+par(cex = 0.8)
+plotEigengeneNetworks(module_eigengenes_trait, "",
+    marDendro = c(0, 4, 1, 2), marHeatmap = c(3, 4, 1, 2), cex.lab = 0.8,
+    xLabelsAngle = 90
+)
+
+
+sizeGrWindow(6, 6)
+par(cex = 1.0)
+plotEigengeneNetworks(MET, "Eigengene dendrogram",
+    marDendro = c(0, 4, 2, 0),
+    plotHeatmaps = FALSE
+)
+
+par(cex = 1.0)
+plotEigengeneNetworks(module_eigengenes_trait, "Eigengene adjacency heatmap",
+    marHeatmap = c(3, 4, 2, 2),
+    plotDendrograms = FALSE, xLabelsAngle = 90
+)
+
+# Export to Cytoscape ------------------------------------------------
+
+# !!!!!!!!!!! NEED to run WGCNA locally before for this to work !!!!!!!!!!!!!
+TOM <- readRDS(paste(etiology, "/TOM_", etiology, ".RDS", sep = ""))
+
+RCy3::cytoscapePing()
+if (!grepl("status: Installed", RCy3::getAppStatus("stringApp"))) RCy3::installApp("stringApp")
+
+# Select the module of interest
+modules <- c("tan", "darkred")
+probes <- names(data_expression)
+in_module <- is.finite(match(module_colors, modules))
+mod_probes <- probes[in_module]
+# Select the corresponding Topological Overlap
+mod_TOM <- TOM[in_module, in_module]
+
+dimnames(mod_TOM) <- list(mod_probes, mod_probes)
+
+# Export the network into edge and node list files Cytoscape can read
+cyt <- exportNetworkToCytoscape(mod_TOM,
+    edgeFile = paste("CytoscapeInput-edges-", paste(modules, collapse = "-"), ".txt", sep = ""),
+    nodeFile = paste("CytoscapeInput-nodes-", paste(modules, collapse = "-"), ".txt", sep = ""),
+    weighted = TRUE,
+    threshold = 0.08,
+    nodeNames = mod_probes,
+    nodeAttr = module_colors[in_module]
+)
+
+edges <- read.table(paste0("CytoscapeInput-edges-", paste(modules, collapse = "-"), ".txt"), sep = "")
+edges <- edges[, c(1:3)]
+colnames(edges) <- c("source", "target", "weight")
+RCy3::createNetworkFromDataFrames(edges = edges, title = paste0("module", " ", paste(modules, collapse = "-")))
